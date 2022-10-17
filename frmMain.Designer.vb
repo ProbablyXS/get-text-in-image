72c3c2bdf2216b10ab6e92971487f4b32d9983aa
Probably <99107085+ProbablyXS@users.noreply.github.com>
2022-10-17 22:45:01 +0200

    Add files via upload

---
+++ frmMain.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmdOpen = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmdOpen
+        '
+        Me.cmdOpen.Location = New System.Drawing.Point(553, 34)
+        Me.cmdOpen.Name = "cmdOpen"
+        Me.cmdOpen.Size = New System.Drawing.Size(92, 23)
+        Me.cmdOpen.TabIndex = 0
+        Me.cmdOpen.Text = "Ouvrir"
+        Me.cmdOpen.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(142, 149)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(503, 294)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(46, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(402, 131)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(454, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(553, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Ouvrir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(657, 455)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.cmdOpen)
+        Me.Name = "frmMain"
+        Me.Text = "Tesseract OCR .NET"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents cmdOpen As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Private WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,96 @@
+﻿Imports Tesseract
+Imports System.Runtime.InteropServices
+Imports System.Drawing.Imaging
+
+Public Class frmMain
+
+    Const DWMWA_EXTENDED_FRAME_BOUNDS = 9&      ' methode precise 
+    <DllImport("dwmapi.dll")>      ' methode precise
+    Private Shared Function DwmGetWindowAttribute(ByVal hwnd As IntPtr, ByVal dwAttribute As Integer, <Out> ByRef pvAttribute As RECT, ByVal cbAttribute As Integer) As Integer
+    End Function
+
+    Private Structure RECT
+
+        Public left, top, right, bottom As Integer
+
+    End Structure
+
+    Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String,
+    ByVal lpWindowName As String) As IntPtr
+
+    Dim hWnd As IntPtr
+    Dim ox, oy, window_h, window_w As Integer
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        PictureBox1.Dispose()
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub cmdOpen_Click(sender As Object, e As EventArgs) Handles cmdOpen.Click
+
+        hWnd = FindWindow("GLFW30", vbNullString)
+
+        If hWnd <> 0 Then
+            Dim wr As New RECT
+
+            DwmGetWindowAttribute(hWnd, DWMWA_EXTENDED_FRAME_BOUNDS, wr, Marshal.SizeOf(wr))
+            Label1.Text = "PokeMMO started!"
+            ox = wr.left
+            oy = wr.top
+            window_h = wr.bottom - wr.top
+            window_w = wr.right - wr.left
+
+        End If
+
+        Dim myBmp As New Bitmap(window_w, window_h)
+        Dim g As Graphics = Graphics.FromImage(myBmp)
+        g.CopyFromScreen(ox, oy, 0, 0, myBmp.Size)
+
+
+        Dim CropRect As New Rectangle(0.002 * window_w, 0.04 * window_h, 110, 20) '298, 681
+        Dim CropImage = New Bitmap(CropRect.Width, CropRect.Height)
+        Dim grp As Graphics = Graphics.FromImage(CropImage)
+        grp.DrawImage(myBmp, New Rectangle(0, 0, CropRect.Width, CropRect.Height), CropRect, GraphicsUnit.Pixel)
+        myBmp.Dispose()
+
+        CropImage.Save("test.png")
+
+        Dim imgPix = PixConverter.ToPix(CropImage)
+
+        Dim test As New TesseractEngine("testdata", "eng", EngineMode.Default)
+
+        Dim page = test.Process(imgPix)
+
+        Dim text = page.GetText()
+
+        Dim iter = page.GetIterator()
+
+        TextBox1.Clear()
+
+        TextBox1.Text += text.Trim()
+
+
+        grp.Dispose()
+        CropImage.Dispose()
+        g.Dispose()
+        myBmp.Dispose()
+        imgPix.Dispose()
+        test.Dispose()
+
+    End Sub
+
+End Class
